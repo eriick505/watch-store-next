981f86183be91dc0cc0724bf9b943b33ca0433b6
Erick <santoserick9@gmail.com>
2021-10-23 13:39:45 -0300

    Filter product and show error message when there are no products

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,50 @@
+import { useEffect, useState } from 'react';
 import { useFetchProducts } from '../hooks/useFetchProducts';
 
 import Search from '../components/Search';
 import ProductCard from '../components/ProductCard';
 
 export default function Home() {
+  const [term, setTerm] = useState('');
+  const [localProducts, setLocalProducts] = useState([]);
+
   const { products, error } = useFetchProducts();
 
-  const renderProductListOrMessage = () => {
-    if (products.length === 0) {
-      return <h4 data-testid="no-products">No products</h4>;
-    }
+  useEffect(() => {
+    if (term === '') return setLocalProducts(products);
 
-    return products.map((product) => {
-      return <ProductCard key={product.id} product={product} />;
-    });
+    const productContainsTerm = ({ title }) =>
+      title.toLowerCase().includes(term.toLocaleLowerCase());
+
+    const productsFiltered = products.filter(productContainsTerm);
+
+    setLocalProducts(productsFiltered);
+  }, [products, term]);
+
+  const renderProductListOrMessage = () => {
+    const isNoTContainsProduct = localProducts.length === 0 && !error;
+
+    if (isNoTContainsProduct) return <h4 data-testid="no-products">No products</h4>;
+
+    return localProducts.map((prod) => <ProductCard key={prod.id} product={prod} />);
   };
+
+  const renderErrorMessage = () => {
+    if (!error) return null;
+
+    return <h4 data-testid="server-error">Server is down</h4>;
+  };
+
+  const doSearch = (term) => setTerm(term);
 
   return (
     <main data-testid="product-list" className="my-8">
-      <Search />
+      <Search doSearch={doSearch} />
       <div className="container mx-auto px-6">
         <h3 className="text-gray-700 text-2xl font-medium">Wrist Watch</h3>
         <span className="mt-3 text-sm text-gray-500">200+ Products</span>
         <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-6">
+          {renderErrorMessage()}
           {renderProductListOrMessage()}
         </div>
       </div>
