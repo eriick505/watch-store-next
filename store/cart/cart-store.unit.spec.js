2ef14ac8a6bd33eda207c96a62265f0b2c55527c
Erick <santoserick9@gmail.com>
2021-10-26 23:10:04 -0300

    addToCart Test

--- store/cart/cart-store.unit.spec.js
+++ store/cart/cart-store.unit.spec.js
@@ -5,10 +5,14 @@ import { useCartStore } from './';
 describe('Cart Store', () => {
   let server;
   let result;
+  let toggle;
+  let addProduct;
 
   beforeEach(() => {
     server = makeServer({ environment: 'test' });
     result = renderHook(() => useCartStore()).result;
+    toggle = result.current.actions.toggle;
+    addProduct = result.current.actions.addProduct;
   });
 
   afterEach(() => {
@@ -28,24 +32,29 @@ describe('Cart Store', () => {
   it('should add 2 products to the list', async () => {
     const products = server.createList('product', 2);
 
-    const { addProduct } = result.current.actions;
-
-    for (const product of products) {
-      act(() => addProduct(product));
-    }
+    products.forEach((product) => act(() => addProduct(product)));
 
     expect(result.current.state.products).toHaveLength(2);
   });
 
-  it('should toggle open state', async () => {
-    const { toggle } = result.current.actions;
+  it('should not add same product twice', () => {
+    const product = server.create('product');
 
+    act(() => addProduct(product));
+    act(() => addProduct(product));
+
+    expect(result.current.state.products).toHaveLength(1);
+  });
+
+  it('should toggle open state', async () => {
     expect(result.current.state.open).toBe(false);
+    expect(result.current.state.products).toHaveLength(0);
 
     act(() => toggle());
     expect(result.current.state.open).toBe(true);
 
     act(() => toggle());
     expect(result.current.state.open).toBe(false);
+    expect(result.current.state.products).toHaveLength(0);
   });
 });
