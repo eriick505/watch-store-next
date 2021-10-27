4dbfd3cd4bbc0876150768748dae062673e1045a
Erick <santoserick9@gmail.com>
2021-10-26 23:09:16 -0300

    handle product

--- store/cart/index.js
+++ store/cart/index.js
@@ -5,16 +5,22 @@ const initialState = {
   products: [],
 };
 
+const handleProduct = (store, product) => {
+  if (store.state.products.includes(product)) return store.state.products;
+
+  return [...store.state.products, product];
+};
+
 export const useCartStore = create((set) => ({
   state: {
     ...initialState,
   },
   actions: {
-    toggle: () => set((store) => ({ state: { open: !store.state.open } })),
+    toggle: () => set((store) => ({ state: { ...store.state, open: !store.state.open } })),
     reset: () => set((store) => ({ state: { ...initialState } })),
     addProduct: (product) =>
       set((store) => ({
-        state: { ...store.state, products: [...store.state.products, product] },
+        state: { ...store.state, products: handleProduct(store, product) },
       })),
   },
 }));
